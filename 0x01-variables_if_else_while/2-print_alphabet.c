5c1ef20ff43ed35697e8c5936e251372c2c3d0c1
Hope-Kerubo <kerubohopes@gmail.com>
2022-09-09 18:47:06 -0700

    2. I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main is used to run the main program
+ *
+ * Return will return zero if no error and non zero if an error is found
+*/
+int main(void)
+{
+    int alphabet;
+
+    alphabet = 'a';
+    while (alphabet <= 'z')
+    {
+        putchar(alphabet);
+        alphabet++;
+    }
+    putchar('\n');
+    return (0);
+}
+
